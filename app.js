1b83e5b19b21fe2c648785f5781625d3acb999e4
aaron <aaronmlucia@gmail.com>
2021-10-29 17:26:51 -0400

    uploading, completed both exercises

---
+++ app.js
@@ -0,0 +1,25 @@
+// ES5 map callback
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//         return val * 2;
+//     });
+// }
+
+//ES2015 solution
+
+const double = (arr => arr.map(((val) => val * 2)));
+
+//replace all functions in the following code with arrow functions
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//         return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//         return square % 2 === 0;
+//     });
+//     return evens;
+// }
+
+const squareAndFindEvens = (numbers => numbers.map((num => num ** 2)).filter((square => square % 2 ===0)));
